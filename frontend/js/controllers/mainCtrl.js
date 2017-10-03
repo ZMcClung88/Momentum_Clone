4c6cd24069f94e78e857cf415e7f3c2149f1ff8b
Zac McClung <zac.mcclung370@gmail.com>
2017-10-03 11:02:15 -0600

    added prompt with personal greeting

--- frontend/js/controllers/mainCtrl.js
+++ frontend/js/controllers/mainCtrl.js
@@ -1,34 +1,18 @@
 angular.module('app').controller('mainCtrl', function($scope, $rootScope, backgroundService, quotesService, weatherService) {
 
-
+  ////////////// GREETING //////////////
+  function greeting() {
+    let customerName = prompt("Please enter your name...");
+    if(customerName != null) {
+      document.getElementById("greeting-personal").innerHTML = "Good afternoon, " + customerName + ".";
+    }
+  }
+  greeting();
 
   ////////////// TIME //////////////
   $scope.time = moment().format('h:mm ');
 
-  ////////////// GET NAME //////////////
-  $scope.getName = function (name) {
-    // console.log('name', name);
-    $scope.name = name;
-  }
-
-  ////////////// GREETING //////////////
-  // $('body').keypress(function(key) {
-  //   if(key.which == 13){
-  //     console.log('you hit enter!')
-  //     $('#input').hide() && $('#greeting-personal').show();
-  //   }
-  // });
-
   ////////////// QUOTE HOVER //////////////
-  // $("#quote").mouseover(function() {
-  //   console.log('mouse in the house!');
-  //   $("#author").removeClass('quote-author');
-  // });
-  // $("#quote").mouseleave(function() {
-  //   console.log('mouse out of the house!');
-  //   $("#author").addClass('quote-author');
-  // });
-
   $(function() {
     $("#quote").hover(function(){
       $("#author").fadeIn("slow");
@@ -46,22 +30,17 @@ angular.module('app').controller('mainCtrl', function($scope, $rootScope, backgr
     let images = backgroundService.backgroundImages;
     let randomNumber = Math.floor(Math.random() * images.length);
     let randomImage = images[randomNumber];
-    // console.log(randomImage);
     return {
       "background-image": 'url(' + randomImage + ')'
     };
   };
   $rootScope.backgroundImages();
-  // console.log($scope.backgroundImages());
 
   ////////////// WEATHER //////////////
   weatherService.getWeather().then(function(response) {
     $scope.city = response.city.name;
-    // console.log($scope.city);
     $scope.weather = Math.floor(response.list[0].main.temp);
-    // console.log($scope.weather);
     $scope.icon = response.list[0].weather[0].icon + '.svg';
-    // console.log($scope.icon);
   });
 
   //////////// QUOTES //////////////
